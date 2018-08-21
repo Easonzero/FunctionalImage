9bb45838bc402d8f192322b71cf512d2c27f799d
wangyi <wangyi@megvii.com>
2018-08-21 10:44:43 +0800

    merge

--- test/test.js
+++ test/test.js
@@ -7,14 +7,14 @@ image.src = './test.jpeg';
 createDatabase('dbcolors', 'float', {white:1.,black:0.});
 
 let cache = -1;
-let w = 11, h = 11;
+let w = h = 11;
 let max = w * w / 4;
 let min = (w - 4) * (w - 4) / 4;
-let c = Math.floor(11 / 2);
+let c = Math.floor(w / 2);
 
 image.onload = () => {
     let container = pure(image)
-        .join((r,v)=>(x,y)=>r*(y*2+x)/(y*2+x+1)+v/(y*2+x+1), [w, h], 'RGB');
+        .join((r,v) => (x,y)=>r*(y*2+x)/(y*2+x+1)+v/(y*2+x+1), [w, h], 'RGB');
     container
         .output()
         .then(x => cache = x);
@@ -43,6 +43,10 @@ function getPointOnCanvas(canvas, x, y) {
 
 canvas.onclick = (e) => {
     let pos = getPointOnCanvas(canvas,e.pageX,e.pageY);
+    w = h -= 2;
+    max = w * w / 4;
+    min = (w - 4) * (w - 4) / 4;
+    c = Math.floor(w / 2);
     if(cache != -1)
         pure(cache)
         .bind(v => x => y => {
